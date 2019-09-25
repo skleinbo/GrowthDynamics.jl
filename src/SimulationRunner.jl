9c1bfb73482c76842a9b04d76a331e37778c9a65
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2019-09-25 08:59:29 +0200

    WIP. Mostly working, but not stable enough.

--- src/SimulationRunner.jl
+++ src/SimulationRunner.jl
@@ -3,6 +3,7 @@ module SimulationRunner
 Helper functions to save results and parameters
 of a run.
 =#
+using MacroTools
 using Distributed
 using JSON
 using DataFrames
@@ -11,7 +12,9 @@ import GrowthDynamics.AnalysisMethods: timeseries
 export  get_last_file_number,
         json_parameters,
         _run_sim_conditional!,
-        repeated_runs
+        repeated_runs,
+		@parameters,
+		setup_simulation_environment
 
 
 function get_last_file_number(path::AbstractString)
@@ -47,26 +50,256 @@ function json_parameters(p)
         throw(err)
     end
 end
-## END helper
+
+include("ParameterTemplate.jl")
+
+mutable struct Job
+	id::Int
+	job::Base.Generator
+	status::Symbol ## Either of :RUNNABLE, :RUNNING, :DONE, :FAILED, :PAUSED
+end
+Job(job::Base.Generator) = Job(0, job, :RUNNABLE)
+
+mutable struct Worker
+	pid::Int
+	commands::RemoteChannel
+	consumer::Future
+end
+Worker(pid::Int) = Worker(pid, RemoteChannel(()->Channel{Any}(Inf)), Future())
+
+mutable struct JobRunner
+	workers::Vector{Worker} ## Which workers does the runner utilize?
+
+	jobs::Vector{Job}
+
+	running::Bool
+
+	jobs_channel::RemoteChannel ## Channel to fill with parameters for every run
+	results_channel::RemoteChannel	 ## Channel to be filled with results
+
+	feeder_task::Task
+end
+
+function descend_dicts(l, r)
+	if isa(l, Dict) && isa(r, Dict)
+		merge(descend_dicts, l, r)
+	else
+		return r
+	end
+end
+
+
+macro parameters(ex)
+	ex = MacroTools.prewalk(rmlines, ex)
+	# @show ex
+	# @show ex.head ex.args length(ex.args)
+	if length(ex.args) != 2 || ex.head != :for
+		error("Usage: @parameters for [...] end")
+	end
+	parameters = map(x->(x.args[1], x.args[2]), ex.args[1].args)
+	template = ex.args[2].args[1]
+	base_template = length(ex.args[2].args) >= 2 ? ex.args[2].args[2] : SimulationRunner.parameter_template
+
+	gen_expr = Expr(:generator, :(merge(SimulationRunner.descend_dicts, $base_template, $template)), (ex.args[1].args)...)
+
+	quote
+		Job($(esc(gen_expr)))
+	end
+end
+
+
+
+module tmp
+	import ..SimulationRunner
+	p = (SimulationRunner.@parameters for N = [1,2,3], mu in [1e-2]
+		Dict(
+			:dyn => Dict(
+				:T => 10*N
+			),
+			:simulation => Dict(
+				:N => N
+			)
+		)
+		parameter_template
+	end)
+end
+
+
+function setup_simulation_environment(num_cpu::Integer=Sys.CPU_THREADS)
+	workers = addprocs(num_cpu)
+	setup_simulation_environment(workers)
+end
+
+function setup_simulation_environment(workers::Vector)
+	## Code loading on the workers/master
+	@show workers
+	@eval Main begin
+		using Distributed
+		using DataFrames
+		using BSON
+		import Base.Iterators: repeated, flatten, product
+		@everywhere $workers begin
+			# import Serialization: serialize
+			import ProgressMeter
+			import Printf: @sprintf
+			using GrowthDynamics
+			using GrowthDynamics.TumorConfigurations
+		end
+	end
+	jr = JobRunner(map(pid->Worker(pid), workers),
+		Job[],
+		false,
+		RemoteChannel(()->Channel{Any}(Inf)),
+		RemoteChannel(()->Channel{Any}(Inf)),
+		Task(nothing))
+	jr.feeder_task = feeder(jr)
+	for worker in 1:length(workers)
+		start_job_processing(jr, worker)
+	end
+
+	return jr
+end
+
+function process_jobs(jobs::RemoteChannel, results::RemoteChannel, commands::RemoteChannel)
+	run = false
+	job = nothing
+	try
+	while true
+		if isready(commands)
+			cmd = take!(commands)
+			if cmd == :RUN
+				@info "Running."
+				run = true
+			elseif cmd == :ABORT || !isopen(results) || !isopen(jobs)
+				@info "Aborting."
+				break
+			elseif cmd == :PAUSE
+				@info "Pausing."
+				run = false
+			end
+		end
+		if run && isopen(jobs)
+			job = take!(jobs)
+			@info "Received a job on $(myid())."
+				SimulationRunner._run_sim_conditional!(
+					job[:simulation],
+					job[:dyn],
+					results
+				    )
+		else
+			@info "Waiting for command..."
+			wait(commands)
+		end
+	end
+	catch e
+		@error e
+		@info job
+		rethrow(e)
+	end
+	return :SUCCESS
+end
+
+function start_job_processing(runner::JobRunner, id::Int)
+	pid = runner.workers[id].pid
+	runner.workers[id].consumer = @spawnat pid begin
+		@async SimulationRunner.process_jobs(runner.jobs_channel, runner.results_channel, runner.workers[id].commands)
+	end
+	@info "Consumer task launched on $pid."
+end
+
+function submit!(runner::JobRunner, job::Job)
+	try
+		job.id = maximum(map(x->x.id, runner.jobs)) + 1
+	catch
+		job.id = 1
+	end
+
+	push!(runner.jobs, job)
+	nothing
+end
+
+function feeder(runner::JobRunner)
+	@task begin
+		while runner.running
+			job_id = findfirst(x->x.status == :QUEUING, runner.jobs)
+			if job_id != nothing
+				for job in runner.jobs[job_id].job
+					if !isopen(runner.jobs_channel)
+						break
+					end
+					put!(runner.jobs_channel, job)
+					@info "Fed $job_id into queue."
+					yield()
+				end
+				@info "Setting $job_id to :RUNNING"
+				runner.jobs[job_id].status = :RUNNING
+			end
+			sleep(0.5)
+		end
+	end
+end
+
+function run!(runner::JobRunner)
+	for j in runner.jobs
+		if j.status == :RUNNABLE
+			@info "Setting job to :QUEUING"
+			j.status = :QUEUING
+		end
+	end
+	if istaskdone(runner.feeder_task)
+		runner.feeder_task = feeder(runner)
+	end
+	for w in 1:length(runner.workers)
+		if (remotecall_fetch(()->!isdefined(Main, :process_task) || istaskdone(process_task), w))
+			@info "Starting consumer task on $(w)"
+			start_job_processing(runner, w)
+		end
+		put!(runner.workers[w].commands, :RUN)
+	end
+	runner.running = true
+	try
+		schedule(runner.feeder_task)
+		@info "Feeder task started."
+	catch err
+		@info "Replacing job feeder."
+		runner.feeder_task = feeder(runner)
+	end
+end
+
+stop!(runner::JobRunner, id) = begin
+	if 0 < id <= length(runner.workers)
+		put!(runner.workers[id].commands, :ABORT)
+	end
+end
+
+start!(runner::JobRunner, id) = begin
+	if 0 < id <= length(runner.workers)
+		put!(runner.workers[id].commands, :RUN)
+	end
+end
+
 
 
 function _run_sim_conditional!(
-    dyn!,
-    obs,
-    setup,
-    params;
+	sim_params,
+	dyn_params,
     results_channel,
-    sweep=0,
-    abort=(s,t)->false,
-    verbosity=0
     )
 
-    verbosity>1 && println(params)
+	verbosity = sim_params[:verbosity]
+	dyn! = sim_params[:dynamics]
+	setup = sim_params[:setup]
+	sweep = sim_params[:sweep]
+	obs = sim_params[:obs]
+	abort = sim_params[:abort]
 
-    state = setup()
-    max_T = params[:T]
+
+	verbosity>1 && println(params)
+
+    state = Base.invokelatest(setup)
+    max_T = dyn_params[:T]
     if sweep>0
-        dyn!(state;params...,T=sweep)
+        dyn!(state;dyn_params...,T=sweep)
     end
 
     k = 2
@@ -74,7 +307,7 @@ function _run_sim_conditional!(
 
     obs_callback(s,t) = obs(X,s,t)
 
-    dyn!(state;params...,T=max_T,callback=obs_callback,abort=abort)
+    dyn!(state;dyn_params...,T=max_T,callback=obs_callback,abort=abort)
 
     if isempty(X)
         if isdefined(Main,:STRICT) && Main.STRICT
@@ -84,11 +317,11 @@ function _run_sim_conditional!(
             @warn ("No observables collected!")
         end
     end
-    put!(results_channel, (:N => params[:N], :s => params[:s], :mu => params[:mu],
-     :d => params[:d], :f0 => params[:f], X))
+    put!(results_channel, (:N =>sim_params[:N], :s => dyn_params[:s], :mu => dyn_params[:mu],
+     :d => dyn_params[:d], :f => dyn_params[:f], X))
 end
 
-
+"To be deprecated"
 function repeated_runs(N::Integer, dyn!, obs, setup, parameters;
      results_channel,
      sweep=0,
