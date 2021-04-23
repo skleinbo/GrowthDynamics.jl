fdefb4d14e18201f787ae815f180ef730e420d83
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2021-04-23 13:17:05 +0200

    documented Geometry.jl

--- src/Geometry.jl
+++ src/Geometry.jl
@@ -1,9 +1,21 @@
+## Defines a plane in three dimensional Euclidean space
+## given by the origin `p`, and three vectors `u,v` in the plane
+## and `w` perpendicular to it.
+## `u,v,w` are not independent, but filled in by the constructors.
+
 struct Plane
     p
     u
     v
     w
 end
+
+"""
+    Plane(p, w)
+
+Construct a plane with origin `p` and normal `w`.
+`w` will be normalized automatically.
+"""
 function Plane(p, w)
     if length(p)!=3 || length(w)!=3
         throw(ArgumentError("Arguments must be vectors of length 3."))
@@ -23,6 +35,12 @@ function Plane(p, w)
     v = cross(u, w)
     return Plane(SVector{3}(p), SVector{3}(u), SVector{3}(v), SVector{3}(w))
 end
+"""
+    Plane(p, u,v)
+
+Construct a plane with origin `p` spanned by `u,v`.
+Vectors will be normalized automatically.
+"""
 function Plane(p, u,v)
     if length(p)!=3 || length(u)!=3 || length(v)!=3
         throw(ArgumentError("Arguments must be vectors of length 3."))
@@ -36,6 +54,11 @@ function Plane(p, u,v)
     return Plane(SVector{3}(p), SVector{3}(u), SVector{3}(v), SVector{3}(w))
 end
 
+"""
+    euclidean_dist(q, P::Plane)
+
+Euclidean distance `| (q-p).w |` of point `q` from plane `P`.
+"""
 function euclidean_dist(q, P::Plane)
     abs(dot((q - P.p), P.w))
 end
