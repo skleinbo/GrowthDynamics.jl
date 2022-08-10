564503fb2bee32a9f5fc3dda337c62f67d8dbf44
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2022-08-10 13:39:34 +0200

    Tests for FCCLattice
    Additional tests for index = coord^{-1}

--- test/latticestests.jl
+++ test/latticestests.jl
@@ -10,6 +10,10 @@ using GeometryBasics
     @test coord(lattice, (3,)) == 3*lattice.a
     @test coord(lattice, CartesianIndex(3)) == 3/2
     @test index(lattice, 0.3) == CartesianIndex(1)
+    I = CartesianIndices(lattice.data)
+    C = coord.(Ref(lattice), I)
+    I′ = index.(Ref(lattice), C)
+    @test all(I .== I′)
     @testset "Neighbors" begin
         @test neighbors(lattice, (3,)) == [CartesianIndex(2), CartesianIndex(4)]
         @test nneighbors(lattice, (3,)) == 2
@@ -34,6 +38,10 @@ end
     @test (coord(lattice, (4,4)) .≈ ((3+1/2)*lattice.a, 3*sin(pi/3)*lattice.a)) |> all
     @test (coord(lattice, CartesianIndex(4,4)) .≈ ((3+1/2)*lattice.a, 3*sin(pi/3)*lattice.a)) |> all
     @test Tuple(index(lattice, ((3+1/2)*lattice.a+0.01, 3*sin(pi/3)*lattice.a+0.02))) == (4,4)
+    I = CartesianIndices(lattice.data)
+    C = coord.(Ref(lattice), I)
+    I′ = index.(Ref(lattice), C)
+    @test all(I .== I′)
     @testset "Neighbors" begin
         @test nneighbors(lattice, (1,1)) == 2
         @test nneighbors(lattice, (5,5)) == 2
@@ -56,6 +64,10 @@ end
     @test (coord(lattice, (5,5,5)) .≈ lattice.a.*Point3f0(4,4,4)) |> all
     @test (coord(lattice, CartesianIndex(4,4,4)) .≈ lattice.a.*(3,3,3)) |> all
     @test Tuple(index(lattice, (3,4.5,2.1))) == (7,10,5)
+    I = CartesianIndices(lattice.data)
+    C = coord.(Ref(lattice), I)
+    I′ = index.(Ref(lattice), C)
+    @test all(I .== I′)
     @testset "Neighbors" begin
         @test nneighbors(lattice, (1,1,1)) == 3
         @test nneighbors(lattice, (5,5,5)) == 3
@@ -64,6 +76,36 @@ end
     @test dist(lattice, CartesianIndex(1,1,0), CartesianIndex(5,5,0)) ≈ lattice.a*sqrt(2)*4
     @test (lattice[3,3,3] = 1) == 1
     @test density(lattice, (3,2,3)) == 1/6
+    I = filter(x->!out_of_bounds(x, size(lattice)), neighbors(lattice, (2,2,2)))
+    lattice[I] .= 1
+    @test lattice[I] == fill(1, length(I))
+    @test density(lattice, (2,2,2)) == 1.0
+end
+# FCCLattice
+@testset "FCCLattice" begin
+    lattice = Lattices.FCCLattice(1/2, fill(0, 10,10,10))
+    @test size(lattice) == (10,10,10)
+    @test length(lattice) == 10^3
+    @test coord(lattice, (1,1,1)) == Point3f0(0.0, 0.0, 0.0)
+    @test (coord(lattice, (1,2,4)) .≈ lattice.a.*Point3f0(0,1+1/2,1+1/2)) |> all
+    @test (coord(lattice, (1,2,5)) .≈ lattice.a.*Point3f0(0,1,2)) |> all
+    @test (coord(lattice, (1,3,4)) .≈ lattice.a.*Point3f0(0,2+1/2,1+1/2)) |> all
+    @test (coord(lattice, (1,3,5)) .≈ lattice.a.*Point3f0(0,2,2)) |> all
+    I = CartesianIndices(lattice.data)
+    C = coord.(Ref(lattice), I)
+    I′ = index.(Ref(lattice), C)
+    @test all(I .== I′)
+
+    @test (coord(lattice, CartesianIndex(4,4,4)) .≈ lattice.a.*(1+1/2,3,1+1/2)) |> all
+    @test Tuple(index(lattice, Point3f0(3,4.5,2.1))) == (13, 10, 9)
+    @testset "Neighbors" begin
+        @test_broken  nneighbors(lattice, (1,1,1)) == 3
+        @test nneighbors(lattice, (5,5,5)) == 12
+        @test_broken nneighbors(lattice, (3,3,3)) == 6
+    end
+    @test dist(lattice, CartesianIndex(1,1,1), CartesianIndex(5,5,1)) ≈ lattice.a*sqrt(2^2+4^2)
+    @test (lattice[3,3,3] = 1) == 1
+    @test density(lattice, (3,3,2)) == 1/12
     I = filter(x->!out_of_bounds(x, size(lattice)), neighbors(lattice, (2,2,2)))
     lattice[I] .= 1
     @test lattice[I] == fill(1, length(I))
