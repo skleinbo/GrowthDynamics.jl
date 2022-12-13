5f91c4085814e84c425be3d902722c4c67ee569e
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2022-12-13 07:15:37 +0100

    Exports
    Use @reexport to export all exported symbols from submodules.

--- src/GrowthDynamics.jl
+++ src/GrowthDynamics.jl
@@ -1,30 +1,20 @@
 module GrowthDynamics
 
-export  Lattices,
-        LatticeTumorDynamics
-
-import Printf: @sprintf
-
-using DataFrames
-using FileIO
-using Graphs
-
-linspace(start,stop,length) = range(start, stop=stop, length=length)
-DEBUG = false
+import Reexport: @reexport
 
 include("Lattices.jl")
 include("RoughInterfaces.jl")
 include("Phylogenies.jl")
 include("TumorConfigurations.jl")
-# @reexport using .TumorConfigurations
 include("TumorObservables.jl")
 include("LatticeTumorDynamics.jl")
 include("AnalysisMethods.jl")
 include("PrettyPrinting.jl")
 
-# @reexport using .LatticeTumorDynamics
-# @reexport using .TumorObservables
-# @reexport using .AnalysisMethods
-# @reexport using .Phylogenies
+@reexport using .Lattices
+@reexport using .LatticeTumorDynamics
+@reexport using .Phylogenies
+@reexport using .TumorConfigurations
+@reexport using .TumorObservables
 
 end
